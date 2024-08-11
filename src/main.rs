a41c94398311a22171763c183bde8db37e659254
Desperationis <fork_important@pm.me>
2024-08-11 01:06:01 -0700

    Add working version of steghide

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::process::Command;
+
+fn embed_file(photo_path: &str, embedded_path: &str, passphrase: &str) {
+    Command::new("steghide")
+            .arg("embed")
+            .arg("-cf")
+            .arg(photo_path)
+            .arg("-ef")
+            .arg(embedded_path)
+            .arg("-p")
+            .arg(passphrase)
+            .spawn()
+            .expect("Command failed to start");
+
+
+    println!("Embedded {} into {}", embedded_path, photo_path);
+}
+
+fn extract_file(photo_path: &str, passphrase: &str) {
+    Command::new("steghide")
+            .arg("extract")
+            .arg("-sf")
+            .arg(photo_path)
+            .arg("-p")
+            .arg(passphrase)
+            .spawn()
+            .expect("Command failed to start");
+
+
+    println!("Extracted from {}", photo_path);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let test_path = "test_image.jpg";
+    let data_path = "random_data_input";
+    let passphrase = "yourmom";
+    //embed_file(test_path, data_path, passphrase);
+    extract_file(test_path, passphrase);
 }
