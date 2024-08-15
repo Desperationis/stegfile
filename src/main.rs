09303497375f911f13b7137e1fa25ba82bbcc16e
Desperationis <fork_important@pm.me>
2024-08-14 17:28:30 -0700

    try clap

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,40 @@
 use std::process::Command;
 use std::io::Read;
 use std::fs::{self, File};
+use std::env;
 use std::io::{self, Write};
 use std::path::Path;
 use tempfile::TempDir;
+use std::path::PathBuf;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+}
 
 
 
@@ -216,6 +247,48 @@ fn reconstruct(image_paths: &Vec<&str>, passphrase: &str, output_path: &str) {
 
 
 fn main() {
+    let cli = Cli::parse();
+
+    // You can check the value provided by positional arguments, or option arguments
+    if let Some(name) = cli.name.as_deref() {
+        println!("Value for name: {name}");
+    }
+
+    if let Some(config_path) = cli.config.as_deref() {
+        println!("Value for config: {}", config_path.display());
+    }
+
+    // You can see how many times a particular flag or argument occurred
+    // Note, only flags can have multiple occurrences
+    match cli.debug {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Some(Commands::Test { list }) => {
+            if *list {
+                println!("Printing testing lists...");
+            } else {
+                println!("Not printing testing lists...");
+            }
+        }
+        None => {}
+    }
+
+    /*
+    let args: Vec<String> = env::args().collect();
+
+    let mode = args.next().expect("No mode given.");
+    let image_path = args.next().expect("No folder to search for images.");
+    let input_file = args.next().expect("No input file.");
+    let passphrase = args.next().expect("No passphrase");
+
+
     let _data_path = "random_data_input";
     let passphrase = "yourmom";
     let mut images = Vec::new();
@@ -233,5 +306,5 @@ fn main() {
     println!("The total capacity of your drive is {} bytes", total_space_bytes);
 
     atomizize(_data_path, &images, passphrase);
-    reconstruct(&images, passphrase, "deconstructed");
+    reconstruct(&images, passphrase, "deconstructed");*/
 }
